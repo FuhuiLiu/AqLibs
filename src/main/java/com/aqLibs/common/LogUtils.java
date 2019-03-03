4c9741ce04ffb1a6368448d02273978a8e0c6579
liufuhui <liufuhui@meituan.com>
2019-03-03 16:40:25 +0800

    AppInfoUtils LogUtils PhoneUtils SystemUtils

---
+++ src/main/java/com/aqLibs/common/LogUtils.java
@@ -0,0 +1,190 @@
+package com.aqLibs.common;
+
+/**
+ * author: AqCxBoM
+ * date: On 2019/3/2
+ */
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Set;
+
+public class LogUtils {
+    public enum ELogSwitch{ DISABLE, ENABLE} //日志开关状态枚举类
+    public enum ELogLevel{ //日志输出等级枚举类
+        NORMAL, NORMAL2, VERBOSE, DEBUG, INFO, WARN, ERROR, ASSERT
+    }
+    private final static String mTAG = "AqCxBoM"; //默认TAG
+    private final static String mCT = "Run here!"; //默认CT
+    private final static String WARNING = "Here got a problem!"; //默认WARN字符串
+    private final static String WARNINGPRE =
+            "=====================================WARNINGPRE=====================================";
+    private final static String WARNINGSUF =
+            "=====================================WARNINGSUF=====================================";
+    private static ELogSwitch mLogSwitch = ELogSwitch.ENABLE; //默认日志开关状态为开启
+    private static ELogLevel mNLogLevel = ELogLevel.INFO; //默认日志输出等级为INFO
+
+    private static void logi(String ct){ Log.i(mTAG, ct); }
+    private static void logv(String ct){ Log.v(mTAG, ct); }
+    private static void logw(String ct){ Log.w(mTAG, ct); }
+    private static void loge(String ct){ Log.e(mTAG, ct); }
+    private static void logd(String ct){ Log.d(mTAG, ct); }
+
+    private static void LOGITAG(String tag,String ct){ Log.i(tag, ct); } //以INFO等级输出参数LOG，TAG
+    private static void LOGVTAG(String tag,String ct){ Log.v(tag, ct); } //以VERBOSE等级输出参数LOG，TAG
+    private static void LOGWTAG(String tag,String ct){ Log.w(tag, ct); } //以WARN等级输出参数LOG，TAG
+    private static void LOGETAG(String tag,String ct){ Log.e(tag, ct); } //以ERROR等级输出参数LOG，TAG
+    private static void LOGDTAG(String tag,String ct){ Log.d(tag, ct); } //以DEBUG等级输出参数LOG，TAG
+
+    private static ELogLevel getCurLogLevel() { return mNLogLevel; } //获取当前Log输出等级
+    private static ELogSwitch getLogSwitchStatus() { return mLogSwitch; }; //获取日志输出开关状态
+    private static void setLogSwitchStatus(ELogSwitch switchNow) { mLogSwitch = switchNow; } //设置日志输出开关状态
+    //返回当前日志开关是否关闭
+    private static boolean isDisbleLog() { return getLogSwitchStatus() == ELogSwitch.DISABLE ? true : false; }
+    //获取当前日志输出等是否过界
+    private static boolean isOutLogLevel(ELogLevel lv) {
+        return lv == ELogLevel.ASSERT || lv == ELogLevel.NORMAL2 ? true : false;
+    }
+
+    //以下为可供外界调用的接口
+    //写日志保存到存储卡 /storage/emulated/0/debug.txt
+    public static void write(String msg) {
+        String exPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        msg = msg + "\n";
+        File filePath = new File(exPath, "debug.txt");
+        try {
+            msg = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss", Locale.US).format(new Date())
+                    + ": " + msg;
+            FileOutputStream fos = new FileOutputStream(filePath, true);
+            fos.write(msg.getBytes());
+            fos.flush();
+            fos.close();
+        }
+        catch(Exception v0) {
+            v0.printStackTrace();
+        }
+    }
+    //关闭日志输出功能
+    public static void DisbleLog() { setLogSwitchStatus(ELogSwitch.DISABLE);}
+    //设置日志输出等级
+    public static void SetLogLevel(ELogLevel nLevel){ mNLogLevel = nLevel; }
+    //重置日志输出等级为INFO
+    public static void ResetLogLevel(){ mNLogLevel = ELogLevel.INFO;}
+    //输出警告日志信息
+    public static void DOWARNING() {
+        DOLOGONCE(WARNING, ELogLevel.ERROR);
+    }
+
+    /**
+     打印栈信息，输出格式
+     =====================================WARNINGPRE=====================================
+     调用栈信息
+     =====================================WARNINGSUF=====================================
+     */
+    public static void PrintStack(){
+        Exception ep = new Exception(mTAG);
+        DOLOGONCE(WARNINGPRE, ELogLevel.ERROR);
+        ep.printStackTrace();
+        DOLOGONCE(WARNINGSUF, ELogLevel.ERROR);
+    }
+
+    //以INFO等级输出参数LOG，TAG为AqCxBoM
+    public static void LOGI(String ct){ logi(ct); }
+    //以VERBOSE等级输出参数LOG，TAG为AqCxBoM
+    public static void LOGV(String ct){ logv(ct); }
+    //以WARN等级输出参数LOG，TAG为AqCxBoM
+    public static void LOGW(String ct){ logw(ct); }
+    //以ERROR等级输出参数LOG，TAG为AqCxBoM
+    public static void LOGE(String ct){ loge(ct); }
+    //以DEBUG等级输出参数LOG，TAG为AqCxBoM
+    public static void LOGD(String ct){ logd(ct); }
+
+    //打印Index
+    public static void LOGPATH(int nIndex){ DOLOG("Current Index: " + nIndex); }
+    public static void LOGPATH(String tag, int nIndex){ DOLOG(tag, "Current Index: " + nIndex); }
+
+    /**
+     * 输出默认日志，格式为 AqCxBoM: "Run here!"
+     */
+    public static void DOLOG(){
+        if (!isOutLogLevel(getCurLogLevel()) && !isDisbleLog()){
+            switch(getCurLogLevel()){
+                case INFO: LOGI(mCT); break;
+                case VERBOSE: LOGV(mCT); break;
+                case WARN: LOGW(mCT); break;
+                case ERROR: LOGE(mCT); break;
+                case DEBUG: LOGD(mCT); break; }
+        }
+    }
+    /**
+     * 输出默认TAG日志，格式为 AqCxBoM: ct
+     */
+    public static void DOLOG(String ct){
+        if (!isOutLogLevel(getCurLogLevel()) && !isDisbleLog()){
+            switch(getCurLogLevel()){
+                case INFO: LOGI(ct); break;
+                case VERBOSE: LOGV(ct); break;
+                case WARN: LOGW(ct); break;
+                case ERROR: LOGE(ct); break;
+                case DEBUG: LOGD(ct); break; }
+        }
+    }
+    /**
+     * 输出指定日志，格式为 tag: ct
+     */
+    public static void DOLOG(String tag,String ct){
+        if (!isOutLogLevel(getCurLogLevel()) && !isDisbleLog()){
+            switch(getCurLogLevel()){
+                case INFO: LOGITAG(tag, ct); break;
+                case VERBOSE: LOGVTAG(tag, ct); break;
+                case WARN: LOGWTAG(tag, ct); break;
+                case ERROR: LOGETAG(tag, ct); break;
+                case DEBUG: LOGDTAG(tag, ct); break; }
+        }
+    }
+
+    /**
+     * 以指定TAG\CT\LOGLEVEL，输出日志（日志等级一次性）
+     * @param tag
+     * @param ct
+     * @param lv
+     */
+    public static void DOLOGONCE(String tag, String ct, ELogLevel lv){
+        ELogLevel curLv = getCurLogLevel();
+        SetLogLevel(lv);
+        if (!isOutLogLevel(getCurLogLevel()) && !isDisbleLog()){
+            switch(getCurLogLevel()){
+                case INFO: LOGITAG(tag, ct); break;
+                case VERBOSE: LOGVTAG(tag, ct); break;
+                case WARN: LOGWTAG(tag, ct); break;
+                case ERROR: LOGETAG(tag, ct); break;
+                case DEBUG: LOGDTAG(tag, ct); break; }
+        }
+        SetLogLevel(curLv);
+    }
+
+    /**
+     * 以默认TAG，指定CT\LOGLEVEL，输出日志（日志等级一次性）
+     * @param ct
+     * @param lv
+     */
+    public static void DOLOGONCE(String ct, ELogLevel lv){
+        ELogLevel curLv = getCurLogLevel();
+        SetLogLevel(lv);
+        if (!isOutLogLevel(getCurLogLevel()) && !isDisbleLog()){
+            switch(getCurLogLevel()){
+                case INFO: LOGI(ct); break;
+                case VERBOSE: LOGV(ct); break;
+                case WARN: LOGW(ct); break;
+                case ERROR: LOGE(ct); break;
+                case DEBUG: LOGD(ct); break; }
+        }
+        SetLogLevel(curLv);
+    }
+}
---
+++ src/main/java/com/aqLibs/common/SystemUtils.java
@@ -0,0 +1,326 @@
+package com.aqLibs.common;
+
+/**
+ * author: AqCxBoM
+ * date: On 2019/3/3
+ */
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.text.format.Formatter;
+import android.util.Log;
+
+import android.os.Process;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+
+/**
+ * Created by AqCxBoM on 2017/3/26.
+ */
+
+public class SystemUtils {
+    private static String TAG = "AqCxBoM";
+    public static String mCPU_ABI;
+    public static String mPPATH;
+    private static void LOG(String tag, Object obj)
+    {
+        Log.i(tag, (String)obj);
+    }
+    private static void LOG(Object obj)
+    {
+        Log.i(TAG, (String)obj);
+    }
+
+    //检索已安装程序
+    public static void listInstalledPackage(Context context)
+    {
+        //Object v1_2 = context.getSystemService(Context.ACTIVITY_SERVICE);
+        PackageManager pm = context.getPackageManager();
+        //通过PackageManager检查权限
+        //pm.checkPermission()
+        Iterator it = pm.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES).iterator();
+        while(it.hasNext()) {
+            PackageInfo pi = (PackageInfo)it.next();
+            LOG(pi.packageName);
+        }
+    }
+
+    //通过检索指定目录是否存在su文件来判断机器是否有root权限
+    public static boolean isRoot() {
+        boolean bRet = true;
+        String[] strAry = new String[]{"/system/bin/", "/system/xbin/", "/system/sbin/", "/sbin/", "/vendor/bin/"};
+        int i = 0;
+        try {
+            while (i < strAry.length) {
+                if (new File(strAry[i] + "su").exists()) {
+                    bRet = true;
+                    break;
+                }
+                ++i;
+            }
+        } catch (Exception v0_1) {
+        }
+        return bRet;
+    }
+
+    public static int getPPid(int pid) {
+        int nResult = -1;
+        //注意导入类 import android.os.Process;
+        Class clsProc = Process.class;
+        try {
+            Method md = clsProc.getDeclaredMethod("getParentPid", Integer.TYPE);
+            nResult = (int)md.invoke(null, pid);
+        }
+        catch(Throwable e) {
+            e.printStackTrace();
+        }
+
+        return nResult;
+    }
+    //从 /proc/cpuinfo 中读取CPU信息
+    public static String getCpuInfo() {
+        BufferedReader br;
+        FileReader fr;
+        String str = null;
+        try {
+            fr = new FileReader("/proc/cpuinfo");
+            br = new BufferedReader(fr, 1024);
+            str = br.readLine();
+            br.close();
+            fr.close();
+        }
+        catch(FileNotFoundException v3) {
+        }
+        catch(IOException v6) {}
+
+        String v8 = str != null ? str.substring(str.indexOf(':') + 1).trim() : "";
+
+        return v8;
+    }
+    //获取app标签即程序名
+    public static String getAppLabel(Context pContext) {
+        try {
+            PackageManager pm = pContext.getPackageManager();
+            String pn = pContext.getPackageName();
+            PackageInfo pi = pm.getPackageInfo(pn, PackageManager.GET_ACTIVITIES);
+            String label = pm.getApplicationLabel(pi.applicationInfo).toString();
+            return label;
+        }
+        catch(PackageManager.NameNotFoundException v0) {
+            v0.printStackTrace();
+        }
+
+        return null;
+    }
+    //获取系统SDK版本
+    public static int getSystemVersion() {
+        int nVersion = 0;
+        try {
+            nVersion = Build.VERSION.class.getField("SDK_INT").getInt(null);
+        }
+        catch(Exception v1) {
+            try {
+                Object obj = Build.VERSION.class.getField("SDK").get(null);
+                nVersion = Integer.parseInt((String)obj);
+            }
+            catch(Exception v2) {
+                v2.printStackTrace();
+            }
+        }
+
+        return nVersion;
+    }
+    //获取CPU架构信息
+    public static String getCPUABI() {
+        String cpuabi = "";
+        try {
+            if (new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("getprop ro.product.cpu.abi")
+                    .getInputStream())).readLine().contains("x86")) {
+                cpuabi = "x86";
+            } else
+                cpuabi = "arm";
+        } catch (Exception v1) {
+        }
+        return cpuabi;
+    }
+    //查找指定服务名 遍历服务
+    public static boolean isExistService(Context context, String name) {
+        boolean bRet = false;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        Iterator it = am.getRunningServices(0x7FFFFFFF).iterator();
+        while (it.hasNext()) {
+            ActivityManager.RunningServiceInfo rsi = (ActivityManager.RunningServiceInfo) it.next();
+            if (rsi.service == null) {
+                continue;
+            }
+            String className = rsi.service.getClassName().toString();
+            //LOG(className);
+            if (className.equals(name)) {
+                bRet = true;
+                LOG("MicroMsg.Util", "mAttributeId = " + name + " is running");
+                break;
+            }
+        }
+        if (!bRet)
+            LOG("MicroMsg.Util", "mAttributeId=" + name + " is not running");
+        return bRet;
+    }
+    public static void ergodicProcess(Context context) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        Iterator it = am.getRunningAppProcesses().iterator();
+        while(it.hasNext()) {
+            ActivityManager.RunningAppProcessInfo rapi = (ActivityManager.RunningAppProcessInfo)it.next();
+            int nPid = rapi.pid;    //The pid of this process; 0 if none
+            int nUid = rapi.uid;    //The user id of this process.
+            //当前进程的所有包名 All packages that have been loaded into the process.
+            String[] ppp = rapi.pkgList;
+            //当前进程的包名
+            String processName = rapi.processName;
+            LOG("pid:" + nPid + " uid:" + nUid + " processName:" + processName);
+        }
+    }
+    //检索是否存在指定包名的进程
+    public static boolean isExistProcess(Context context, String Name) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        Iterator it = am.getRunningAppProcesses().iterator();
+        while(it.hasNext()) {
+            ActivityManager.RunningAppProcessInfo rapi = (ActivityManager.RunningAppProcessInfo)it.next();
+            int nPid = rapi.pid;    //The pid of this process; 0 if none
+            int nUid = rapi.uid;    //The user id of this process.
+            //当前进程的所有包名 All packages that have been loaded into the process.
+            String[] ppp = rapi.pkgList;
+            //当前进程的包名
+            String processName = rapi.processName;
+            LOG("pid:" + nPid + " uid:" + nUid + " processName:" + processName);
+            if (processName.equals(Name))
+            {
+                LOG("find match with processName: " + Name);
+                return true;
+            }
+        }
+        return false;
+    }
+    public static String getOSName()
+    {
+        return System.getProperty("os.name");
+    }
+    public static String getVM_Version()
+    {
+        String vmVersion = System.getProperty("java.vm.version");
+        return vmVersion;
+    }
+    public static String getCurrentRuntimeValue2()
+    {
+        String SELECT_RUNTIME_PROPERTY = "persist.sys.dalvik.vm.lib";
+        String LIB_DALVIK = "libdvm.so";
+        String LIB_ART = "libart.so";
+        String LIB_ART_D = "libartd.so";
+        try {
+            Class<?> systemProperties = Class.forName("android.os.SystemProperties");
+            try {
+                Method get = systemProperties.getMethod("get",
+                        String.class, String.class);
+                if (get == null) {
+                    return "WTF?!";
+                }
+                try {
+                    final String value = (String)get.invoke(
+                            systemProperties, SELECT_RUNTIME_PROPERTY,
+                            /* Assuming default is */"Unknow");
+                    if (LIB_DALVIK.equals(value)) {
+                        return "Dalvik";
+                    } else if (LIB_ART.equals(value)) {
+                        return "ART";
+                    } else if (LIB_ART_D.equals(value)) {
+                        return "ART debug build";
+                    }
+
+                    return value;
+                } catch (IllegalAccessException e) {
+                    return "IllegalAccessException";
+                } catch (IllegalArgumentException e) {
+                    return "IllegalArgumentException";
+                } catch (InvocationTargetException e) {
+                    return "InvocationTargetException";
+                }
+            } catch (NoSuchMethodException e) {
+                return "SystemProperties.get(String key, String def) method is not found";
+            }
+        } catch (ClassNotFoundException e) {
+            return "SystemProperties class is not found";
+        }
+    }
+
+    /**
+     * 获取当前系统运行模式
+     * @return 模式字符串
+     */
+    public static String getCurrentRuntimeValue()
+    {
+        String filePath = "/system/build.prop";// 系统内存信息文件
+        String strRead;
+        String[] arrayOfString;
+        try {
+            //读取/system/build.prop文件内容
+            FileReader localFileReader = new FileReader(filePath);
+            BufferedReader localBufferedReader = new BufferedReader(
+                    localFileReader, 8192);
+            do {
+                //挨行判断是否包含字符串persist.sys.dalvik.vm.lib
+                strRead = localBufferedReader.readLine();// 读取meminfo第一行，系统总内存大小
+                if (strRead.contains("persist.sys.dalvik.vm.lib"))
+                    break;
+            }while(true);
+            //"="号分割
+            arrayOfString = strRead.split("=");
+            localBufferedReader.close();
+            //等号右边就是当前系统加载SO
+            return arrayOfString[1].equals("libart.so") ? "ART" : "DALVIK";
+        } catch (IOException e) {
+        }
+        return "未知";
+    }
+    public static String getAvailMemory(Context context) {// 获取android当前可用内存大小
+
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+        am.getMemoryInfo(mi);
+        //mi.availMem; 当前系统的可用内存
+
+        return Formatter.formatFileSize(context, mi.availMem);// 将获取的内存大小规格化
+    }
+
+    public static String getTotalMemory(Context context) {
+        String str1 = "/proc/meminfo";// 系统内存信息文件
+        String str2;
+        String[] arrayOfString;
+        long initial_memory = 0;
+
+        try {
+            FileReader localFileReader = new FileReader(str1);
+            BufferedReader localBufferedReader = new BufferedReader(
+                    localFileReader, 8192);
+            str2 = localBufferedReader.readLine();// 读取meminfo第一行，系统总内存大小
+
+            arrayOfString = str2.split("\\s+");
+//            for (String num : arrayOfString) {
+//                Log.i(str2, num + "\t");
+//            }
+
+            initial_memory = Integer.valueOf(arrayOfString[1]).intValue() * 1024;// 获得系统总内存，单位是KB，乘以1024转换为Byte
+            localBufferedReader.close();
+
+        } catch (IOException e) {
+        }
+        return Formatter.formatFileSize(context, initial_memory);// Byte转换为KB或者MB，内存大小规格化
+    }
+}
